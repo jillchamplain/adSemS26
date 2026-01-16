a2b7e62977a37032b90707aa9a221d015e025e1d
jill <jillian.chapin@mymail.champlain.edu>
2026-01-16 16:02:41 -0500

    Unity Init
    Made the project

---
+++ adSemDestruct/Assets/Scripts/Grid.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] GameObject gridPiecePF;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateGrid(int row, int col)
+    {
+
+    }
+
+
+}
---
+++ adSemDestruct/Assets/Scripts/GridPiece.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class GridPiece : MonoBehaviour
+{
+    [SerializeField] int row;
+    [SerializeField] int col;
+
+}
